87795836dea293560a2561a725ce602ec29c5a3c
Glawall <glawall217@gmail.com>
2025-01-31 15:57:02 +0000

    setup gh pages

--- GoodNeighbour/src/utils/api-client.js
+++ GoodNeighbour/src/utils/api-client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const isDevelopment = import.meta.env.MODE === "development";
 
-const baseURL = isDevelopment ? "/api" : `${import.meta.env.VITE_API_URL}/api`;
+const baseURL = isDevelopment ? "/api" : `${import.meta.env.VITE_API_URL}`;
 
 const api = axios.create({
   baseURL,
